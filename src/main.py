173db7041216d2c3f6de76a786cd1dd417e7bf0d
Kevin Gomulia <kevingomulia@temasek.com.sg>
2025-08-27 14:36:30 +0800

    Changed util -> utils

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 import re
-from .util import is_match
+from utils import is_match
 
 API_KEY = st.secrets["auth"]["api_key"]
 st.set_page_config(page_title="🥕 Fridge Cleaner", layout="wide")
